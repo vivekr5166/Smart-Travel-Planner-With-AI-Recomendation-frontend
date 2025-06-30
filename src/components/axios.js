5324a8e217eceaacdfd1d51f94f85fb1ab47b844
Vivek Kumar <vivekkumar7368956857@gmail.com>
2025-06-30 17:37:16 +0530

    final sumit

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,9 @@
-import axios from "axios"
-const instance = axios.create({
-    baseURL:"https://smart-travel-planner-with-ai.onrender.com"
-    // baseURL:"http://localhost:3000"
+import axios from "axios";
 
-})
-export default instance
+const instance = axios.create({
+  baseURL: "https://smart-travel-planner-with-ai.onrender.com",
+  withCredentials: true  
+  // baseURL: "http://localhost:3000",
+});
+
+export default instance;
